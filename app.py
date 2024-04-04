ed831a8823465ad2226f52de5b7922e6b9682812
fardin79 <fardeenrangrezz4@gmail.com>
2024-04-04 21:15:01 +0530

    prediction pipline added

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from mlproject import logger
-from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
+from flask import Flask,render_template,request
+import os
+import numpy as np
+import pandas as pd
+from mlproject.pipeline.prediction import PredictionPipeline
 
 
+app=Flask(__name__)
 
 
+@app.route("/")
+def home_page():
+    return "<p>Hello, world!</p>"
 
-STAGE_NAME="Data Ingestion stage"
-try:
-    logger.info(f">>>>>> stage {STAGE_NAME} start <<<<<<") 
-    obj=DataIngestionTraningPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
-except Exception as e:
-    logger.exception(e)
-    raise e
+@app.route('/train',methods=['GET'])
+def training():
+    os.system("python main.py")
+    return "Training Successful!"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=8080)
